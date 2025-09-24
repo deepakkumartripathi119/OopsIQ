df8babd50952a1fad1349ff4b2d214de909001de
deepaktripathi119 <deepaktripathi14022005@gmail.com>
2025-09-25 00:03:20 +0530

    changes in globals and layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "OopsIQ | Powered by Google Gemini",
+  title: "OopsIQ",
   description:
     "Generate quizzes from your notes using the AI-powered quiz generator",
 };
